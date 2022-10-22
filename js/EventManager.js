4fb9afe2b009aa612b96362c29d400738548dd8e
Lu-Die-Milchkuh <78442275+Lu-Die-Milchkuh@users.noreply.github.com>
2022-10-22 10:48:58 +0200

    Added EventManager

---
+++ js/EventManager.js
@@ -0,0 +1,21 @@
+"use strict"
+
+// Reading File into memory
+document.querySelector("#file-input").addEventListener("change", (event) => {
+    let reader = new FileReader()
+    const files = event.target.files
+
+    reader.readAsArrayBuffer(files[0])
+    reader.onload = () => {
+        let buffer = reader.result
+        let tempArray = new Uint8Array(buffer)
+        memory.rom = Array.from(tempArray)
+
+    }
+
+})
+
+
+document.querySelector("#speed-slider").addEventListener("input", (event) => {
+    document.querySelector("#speed-out").value = `${document.querySelector("#speed-slider").value}%`
+})
