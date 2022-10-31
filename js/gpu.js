519a56e1c197520ee0ec045e0e7f09300cac0c60
Lu-Die-Milchkuh <78442275+Lu-Die-Milchkuh@users.noreply.github.com>
2022-10-31 10:36:41 +0100

    Updated Project

--- js/gpu.js
+++ js/gpu.js
@@ -2,6 +2,7 @@
 
 import {mmu} from "./mmu.js"
 import {cpu} from "./cpu.js"
+import {screen} from "./screen.js"
 
 export let gpu = {
     vram: new Array(0x2000),
@@ -9,86 +10,23 @@ export let gpu = {
     frame_buffer: new Array(160 * 144),
     clock: 0,
     mode: 2,
-    line: 0
-}
-
-gpu.setMode = (x) => {
-    gpu.mode = x
-    let newSTAT = mmu.read(0xFF41)
-    newSTAT &= 0xFC;
-    newSTAT |= gpu.mode;
-    mmu.write(newSTAT,0xFF41)
-
-    if (gpu.mode < 3) {
-        if (newSTAT & (1 << (3 + gpu.mode))) {
-            cpu.requestInterrupt(1)
-        }
-    }
+    line: 0,
+    frameBuffer: new Array(screen.width * screen.height),   // Store the whole frame
+    tileBuffer: new Array(8),
+    bgTileCache: {}
 }
 
 gpu.update = (cycles) => {
-    gpu.clock += cycles
     let vblank = false
-    switch (gpu.mode) {
-        case 0: // HBLANK
-            if (gpu.clock >= 204) {
-                gpu.clock -= 204
-                gpu.line++;
-                gpu.updateLY();
-                if (gpu.line === 144) {
-                    gpu.setMode(1)
-                    vblank = true
-                    cpu.requestInterrupt(0)
-                    gpu.drawFrame()
-                } else {
-                    gpu.setMode(2)
-                }
-            }
-            break
-
-        case 1: // VBLANK
-            if (gpu.clock >= 456) {
-                gpu.clock -= 456;
-                gpu.line++;
-                if (gpu.line > 153) {
-                    gpu.line = 0
-                    gpu.setMode(2)
-                }
-                gpu.updateLY();
-            }
-            break
-
-        case 2: // SCANLINE OAM
-            if (gpu.clock >= 80) {
-                gpu.clock -= 80
-                gpu.setMode(3)
-            }
-            break
-
-        case 3: // SCANLINE VRAM
-            if (gpu.clock >= 172) {
-                gpu.clock -= 172;
-                gpu.drawScanLine(this.line);
-                gpu.setMode(0)
-            }
-            break
-    }
-
-    return vblank;
+    gpu.clock += cycles
+    let stat = mmu.read(0xFF41)
 }
 
-gpu.updateLY = () => {}
+// Tile Data stored at 0x8000 - 0x97FF, each tile takes 16 Bytes!
+gpu.RenderTiles = () => {}
 
-gpu.update_scanline = () => {
-    let byte = mmu.read(0xFF40)
-
-    if(byte & (1<<0)) {
-        // Background
-    }
-    else if(byte & (1<<5)) {
-        // Window
-    }
-    else if (byte & (1<<2)) {
-        // Sprites
-    }
+gpu.RenderBackground = () => {}
+gpu.RenderWindow = () => {}
+gpu.reset = () => {
+    gpu.clock = 0
 }
--- js/screen.js
+++ js/screen.js
@@ -1,1 +1,22 @@
 "use strict"
+
+export let screen = {
+    canvas: undefined,
+    context: undefined,
+    width: 160,     // Physical Screen Width
+    height: 144,    // Physical Screen Height
+    frequency: 60,
+    pixel_size: 2
+}
+
+screen.colors = [
+    [0xFF, 0xFF, 0xFF], // White
+    [0xAA, 0xAA, 0xAA], // Light Grey
+    [0x55, 0x55, 0x55], // Grey
+    [0x00, 0x00, 0x00]  // Black
+]
+
+screen.init = () => {
+    screen.canvas = document.querySelector("#game-screen")
+    screen.context = screen.canvas.getContext('2d')
+}
