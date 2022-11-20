722d2f7719a793641b6edcae8a4880efbd140c1d
Lu-Die-Milchkuh <78442275+Lu-Die-Milchkuh@users.noreply.github.com>
2022-11-20 14:45:48 +0100

    Removed unused code

--- js/screen.js
+++ js/screen.js
@@ -1,3 +1,23 @@
+/*
+    WebBoy - A GameBoy Emulator written in Javascript
+    Copyright (C) 2022  Lucas Zebrowsky aka Lu-Die-Milchkuh
+
+    Find me on Github: https://github.com/Lu-Die-Milchkuh
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 "use strict"
 
 import {gpu} from "./gpu.js"
@@ -10,13 +30,6 @@ export let screen = {
     //pixel_size: 2,
     content: undefined
 }
-
-/*screen.colors = [
-    [0x00, 0x00, 0x00], // Black
-    [0x55, 0x55, 0x55], // Grey
-    [0xAA, 0xAA, 0xAA], // Light Grey
-    [0xFF, 0xFF, 0xFF], // White
-]*/
 
 screen.init = () => {
     screen.canvas = document.querySelector("#game-screen")
